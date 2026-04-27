e306e7eb5419e3e0b134317bb2ff2106d56b575e
Alberto Diaz <contacto@albertodiazsaez.com>
2026-04-27 05:34:00 +0200

    feat(backDevTest): Base Spring Boot Application

---
+++ src/main/java/com/albertodiazsaez/similarItemApi/SimilarItemApiApplication.java
@@ -0,0 +1,13 @@
+package com.albertodiazsaez.similarItemApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimilarItemApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimilarItemApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albertodiazsaez/similarItemApi/SimilarItemApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albertodiazsaez.similarItemApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarItemApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
